45d34be788587b513000f84fc8c7ec9f1b5059a8
Nedjagang <praneethpakku@gmail.com>
2026-01-12 12:39:17 +0530

    Add GitHub Actions workflow for Next.js build and deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/DeekshaPortfolio',
+  assetPrefix: '/DeekshaPortfolio/',
   images: {
     unoptimized: true,
   },
